65df39180ee4c03e18663a2b4bb2c2f04b0d8859
gifhero <gifhero.mail@gmail.com>
2017-10-18 00:03:50 -0400

    added threading

--- threaded.py
+++ threaded.py
@@ -68,63 +68,65 @@ changed = False
 
 def updateLevels():
     global changed
-    rawCava = raw_input()
-    cava = []
-    i = 0
-    for char in rawCava.split(';'):
-        if char >= 48 and char <= 57: 
-            i = int(char)
-            if value < LEVEL1:
-                levels[i] = 0
-            elif value < LEVEL2:
-                levels[i] = 1
-            elif value < LEVEL3:
-                levels[i] = 2
-            elif value < LEVEL4:
-                levels[i] = 3
-            elif value < LEVEL5:
-                levels[i] = 4
-            elif value < LEVEL6:
-                levels[i] = 5
-            elif value < LEVEL7:
-                levels[i] = 6
-            elif value < LEVEL8:
-                levels[i] = 7
-            else:
-                levels[i] = 8
-        i += 1
-    changed = True
-    print("Changed")
+    while(1):
+        rawCava = raw_input()
+        cava = []
+        i = 0
+        for char in rawCava.split(';'):
+            if char >= 48 and char <= 57: 
+                i = int(char)
+                if value < LEVEL1:
+                    levels[i] = 0
+                elif value < LEVEL2:
+                    levels[i] = 1
+                elif value < LEVEL3:
+                    levels[i] = 2
+                elif value < LEVEL4:
+                    levels[i] = 3
+                elif value < LEVEL5:
+                    levels[i] = 4
+                elif value < LEVEL6:
+                    levels[i] = 5
+                elif value < LEVEL7:
+                    levels[i] = 6
+                elif value < LEVEL8:
+                    levels[i] = 7
+                else:
+                    levels[i] = 8
+            i += 1
+        changed = True
+        print("Changed")
 
 def leds():
     global changed
-    if changed:
-        col(7, levels[0])
-        time.sleep(0.00001)
-        coloff(7)
-        col(11, levels[1])
-        time.sleep(0.00001)
-        coloff(11)
-        col(13, levels[2])
-        time.sleep(0.00001)
-        coloff(13)
-        col(15, levels[3])
-        time.sleep(0.00001)
-        coloff(15)
-        col(29, levels[4])
-        time.sleep(0.00001)
-        coloff(29)
-        col(31, levels[5])
-        time.sleep(0.00001)
-        coloff(31)
-        col(33, levels[6])
-        time.sleep(0.00001)
-        coloff(33)
-        col(35, levels[7])
-        time.sleep(0.00001)
-        coloff(35)
-        changed = False
-        print("Unchanged")
+    while(1):
+        if changed:
+            col(7, levels[0])
+            time.sleep(0.00001)
+            coloff(7)
+            col(11, levels[1])
+            time.sleep(0.00001)
+            coloff(11)
+            col(13, levels[2])
+            time.sleep(0.00001)
+            coloff(13)
+            col(15, levels[3])
+            time.sleep(0.00001)
+            coloff(15)
+            col(29, levels[4])
+            time.sleep(0.00001)
+            coloff(29)
+            col(31, levels[5])
+            time.sleep(0.00001)
+            coloff(31)
+            col(33, levels[6])
+            time.sleep(0.00001)
+            coloff(33)
+            col(35, levels[7])
+            time.sleep(0.00001)
+            coloff(35)
+            changed = False
+            print("Unchanged")
 
 
 getInputs = threading.Thread(target=updateLevels)
